b1685f2332d3e8167cd3f1f6e6f6fc14dab89076
HmUmaxKhan <mukhan.bese22seecs@seecs.edu.pk>
2023-07-20 20:30:33 +0000

    Adding Database tomy project to store the Secrets

--- app.js
+++ app.js
@@ -10,32 +10,56 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
-// mongoose.connect('mongodb://127.0.0.1:27017/secretsDB', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb://127.0.0.1:27017/secretsDB', { useNewUrlParser: true, useUnifiedTopology: true });
 
-// const userSchema = new mongoose.Schema({
-//     email: String,
-//     password: String
-// });
+const userSchema = ({
+    email: String,
+    password: String
+});
 
-// const User = new mongoose.model("User", userSchema);
+const Users = new mongoose.model("User", userSchema);
+
 
 app.get("/", (req, res) => {
     res.render("home");
 })
 
+app.get("/register", (req, res) => {
+    res.render("register");
+})
 
+app.post("/register", (req, res) =>{
+   const newUser = new Users({
+    email: req.body.email,
+    password: req.body.password
+   })
+
+   newUser.save().catch(err => {console.log(err)});
+   res.redirect("/");
+})
 
 app.get("/login", (req, res) => {
     res.render("login");
 })
 
+
 app.get("/secrets", (req, res) => {
     res.render("secrets");
 })
 
-app.get("/register", (req, res) => {
-    res.render("register");
+app.post("/login", (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    Users.findOne({ email: email }).then(user => {
+        if (user && user.password === password) {
+                    res.redirect("/secrets");
+                } else {
+                    res.redirect("/login");
+                }
+    }).catch(err => {console.log(err)});
 })
+
 
 app.get("/submit", (req, res) => {
     res.render("submit");
